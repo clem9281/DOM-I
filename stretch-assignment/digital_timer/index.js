787b2a777fc138305987ea1949886684088cea1a
clem9281 <clem9281@gmail.com>
2019-01-28 15:49:25 -0700

    digital timer is almost done

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,42 @@
+let ms = 0,
+  seconds = 0,
+  counter = 0;
+
+//get elements
+let digits = document.querySelector(".digits");
+let secondTens = document.querySelector("#secondTens");
+let secondOnes = document.querySelector("#secondOnes");
+let msHundreds = document.querySelector("#msHundreds");
+let msTens = document.querySelector("#msTens");
+
+//milliseconds
+setInterval(function() {
+  if (counter <= 10) {
+    if (counter === 10) {
+      digits.style.color = "red";
+    } else if (ms < 1000 && counter < 10) {
+      ms += 10;
+      msHundreds.textContent = Math.floor(ms / 10);
+      msTens.textContent = ms % 10;
+      console.log(Math.floor(ms / 10), ms % 10);
+      if (ms === 1000) {
+        counter++;
+        ms = 0;
+      }
+      //   console.log(ms);
+    }
+  }
+}, 10);
+
+//seconds
+setInterval(function() {
+  if (seconds <= 10) {
+    if (seconds === 10) {
+      digits.style.color = "red;";
+    } else {
+      seconds++;
+      secondOnes.textContent = seconds % 10;
+      secondTens.textContent = Math.floor(seconds / 10);
+    }
+  }
+}, 1000);
