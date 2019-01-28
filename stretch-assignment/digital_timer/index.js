70bde9bd0f49bddc5cf87d807220a634b94f0b33
clem9281 <clem9281@gmail.com>
2019-01-28 16:30:38 -0700

    fixed some logic in digital timer

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,6 +1,5 @@
 let ms = 0,
-  seconds = 0,
-  counter = 0;
+  seconds = 0;
 
 //get elements
 let digits = document.querySelector(".digits");
@@ -11,34 +10,20 @@ let msTens = document.querySelector("#msTens");
 
 //milliseconds
 setInterval(function() {
-  if (counter <= 10) {
-    if (counter === 10) {
-      digits.style.color = "red";
-    } else if (ms < 1000 && counter < 10) {
-      ms += 10;
-      msHundreds.textContent = Math.floor(ms / 10);
-      msTens.textContent = ms % 10;
-      console.log(Math.floor(ms / 100), ms / 10);
-      if (ms === 1000) {
-        counter++;
-        msHundreds.textContent = "00";
-        msTens.textContent = "00";
-        ms = 0;
-      }
-      //   console.log(ms);
+  if (seconds === 10) {
+    digits.style.color = "red";
+  } else if (ms < 1000 && seconds < 10) {
+    ms += 10;
+    msHundreds.textContent = Math.floor(ms / 10);
+    msTens.textContent = ms % 10;
+    if (ms === 1000) {
+      seconds++;
+      msHundreds.textContent = "00";
+      msTens.textContent = "00";
+      ms = 0;
+      secondOnes.textContent = seconds % 10;
+      secondTens.textContent = Math.floor(seconds / 10);
+      console.log(seconds);
     }
   }
 }, 10);
-
-//seconds
-setInterval(function() {
-  if (seconds <= 10) {
-    if (seconds === 10) {
-      digits.style.color = "red;";
-    } else {
-      seconds++;
-      secondOnes.textContent = seconds % 10;
-      secondTens.textContent = Math.floor(seconds / 10);
-    }
-  }
-}, 1000);
